1be204e8868eb6ad2553d4f9cfc7bb664b1296a4
Cratoinyan <umutciloglu06@gmail.com>
2023-01-14 16:32:24 +0300

    add line selection tool

--- app/src/main/java/com/example/app/Commands/Tools/IsHat.kt
+++ app/src/main/java/com/example/app/Commands/Tools/IsHat.kt
@@ -4,24 +4,26 @@ import android.content.Context
 import android.util.Log
 import android.view.MotionEvent
 import android.widget.Toast
-import com.esri.arcgisruntime.data.FeatureTable
+import com.esri.arcgisruntime.data.Feature
 import com.esri.arcgisruntime.data.Geodatabase
 import com.esri.arcgisruntime.data.GeodatabaseFeatureTable
 import com.esri.arcgisruntime.data.QueryParameters
 import com.esri.arcgisruntime.geometry.Envelope
 import com.esri.arcgisruntime.geometry.Point
+import com.esri.arcgisruntime.layers.FeatureLayer
 import com.esri.arcgisruntime.mapping.view.DefaultMapViewOnTouchListener
 import com.esri.arcgisruntime.mapping.view.MapView
-import android.graphics.Point as androidPoint
 
 class IsHat(private val context: Context, private val mapView: MapView, private val geodatabase: Geodatabase):ITool {
     private lateinit var featureTable:GeodatabaseFeatureTable
+    private lateinit var featureLayer: FeatureLayer
+    private val layerName = "Hat"
 
     override fun run() {
         TODO("Not yet implemented")
     }
 
-    override val id = "Find Feature"
+    override val id = "Select Hat"
 
     override val onTouchListener= object : DefaultMapViewOnTouchListener(context,mapView){
         override fun onSingleTapUp(e: MotionEvent?): Boolean {
@@ -29,7 +31,7 @@ class IsHat(private val context: Context, private val mapView: MapView, private 
                 val x = e.x.toInt()
                 val y = e.y.toInt()
                 val newPoint = mapView.screenToLocation(android.graphics.Point(x, y))
-                FFeature(newPoint)
+                FindFeature(newPoint)
             }
             return true
         }
@@ -37,25 +39,37 @@ class IsHat(private val context: Context, private val mapView: MapView, private 
 
     override fun Activate() {
         mapView.onTouchListener = onTouchListener
+
+        mapView.map.operationalLayers.iterator().forEach { layer ->
+            Log.i("LAYER",layer.name)
+            if (layer.name == layerName) {
+                featureLayer = layer as FeatureLayer
+            }
+        }
     }
 
     override fun Deactivate() {
         mapView.onTouchListener = DefaultMapViewOnTouchListener(context, mapView)
+        featureLayer.clearSelection()
     }
 
-    private fun FFeature(point: Point){
-        val extent = Envelope(point,10.0,10.0)
+    private fun FindFeature(point: Point){
+        val extent = Envelope(point,30.0,30.0)
 
-        featureTable = geodatabase.getGeodatabaseFeatureTable("Hat")
+        featureTable = geodatabase.getGeodatabaseFeatureTable(layerName)
         val queryParams = QueryParameters()
         queryParams.spatialRelationship = QueryParameters.SpatialRelationship.INTERSECTS
         queryParams.geometry = extent
-        val result = featureTable.queryFeatureCountAsync(queryParams)
+        val result = featureTable.queryFeaturesAsync(queryParams)
 
         result.addDoneListener {
             try{
-                val resultSet = result.get()
-                if (resultSet > 0){
+                val resultIterator = result.get().iterator()
+                if (resultIterator.hasNext()){
+                    resultIterator.forEach { feature ->
+                        featureLayer.clearSelection()
+                        featureLayer.selectFeature(feature)
+                    }
                     Log.i("RESULT",result.get().toString())
                     Toast.makeText(context,"There is a line here",Toast.LENGTH_LONG).show()
                 }
