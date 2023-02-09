64b673a5f56a5f09eb4b31bb661b0fbc93a09078
Cratoinyan <umutciloglu06@gmail.com>
2023-02-09 18:42:56 +0300

    seperate popup form to add trafo to UI package

--- app/src/main/java/com/example/app/Commands/Tools/PointDrawer.kt
+++ app/src/main/java/com/example/app/Commands/Tools/PointDrawer.kt
@@ -18,6 +18,7 @@ import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol
 import com.example.app.Data.Trafo
 import com.example.app.Managers.DBManager
 import com.example.app.R
+import com.example.app.UI.SaveTrafoPopUp
 import java.lang.Math.floor
 import java.text.SimpleDateFormat
 import java.util.*
@@ -27,8 +28,7 @@ class PointDrawer(private var context: Context, private var mapView: MapView, va
     private val pointGraphicsOverlay = GraphicsOverlay()
     override val id = "Add Point"
     private lateinit var point: Point
-    private var popupWindow: PopupWindow? = null
-
+    private val saveTrafoPopUp = SaveTrafoPopUp(context,dbManager,layout, pointGraphicsOverlay)
     init {
         mapView.graphicsOverlays.add(pointGraphicsOverlay)
     }
@@ -54,7 +54,7 @@ class PointDrawer(private var context: Context, private var mapView: MapView, va
     override fun Deactivate() {
         mapView.onTouchListener = DefaultMapViewOnTouchListener(context,mapView)
         pointGraphicsOverlay.graphics.clear()
-        popupWindow?.dismiss()
+        saveTrafoPopUp.dismiss()
     }
 
     override fun run() {
@@ -64,7 +64,7 @@ class PointDrawer(private var context: Context, private var mapView: MapView, va
     private fun drawPoint(point: Point){
         //clear screen
         pointGraphicsOverlay.graphics.clear()
-        popupWindow?.dismiss()
+        saveTrafoPopUp.dismiss()
 
         val simpleMarkerSymbol = SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, -0xa8cd, 10f)
 
@@ -78,54 +78,6 @@ class PointDrawer(private var context: Context, private var mapView: MapView, va
         pointGraphicsOverlay.graphics.add(pointGraphic)
 
         //pop up menu to ask additional information about the point
-        popUpForm()
-    }
-
-    private fun popUpForm(){
-        //get relevant layout
-        val inflater = context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val view = inflater.inflate(R.layout.save_point_popup,null)
-
-        //show popupwindow TODO: Take a look at bottom sheet dialog
-        val width = LinearLayout.LayoutParams.MATCH_PARENT
-        val height = LinearLayout.LayoutParams.WRAP_CONTENT
-        popupWindow = PopupWindow(view, width, height, true)
-        popupWindow?.showAtLocation(layout,Gravity.BOTTOM,0,0)
-
-        val code = view.findViewById<EditText>(R.id.code_input)
-        val name = view.findViewById<EditText>(R.id.name_input)
-        val type = view.findViewById<EditText>(R.id.type_input)
-        val field = view.findViewById<EditText>(R.id.field_input)
-        val datePicker = view.findViewById<DatePicker>(R.id.date_input)
-
-        //set button events to save to db or close the popup
-        val saveBtn = view.findViewById<Button>(R.id.popup_close_button)
-        saveBtn.setOnClickListener {
-            //get the date
-            val month = datePicker.month
-            val day = datePicker.dayOfMonth
-            val year = datePicker.year
-
-            val cal = Calendar.getInstance()
-            cal.set(year,month,day)
-
-            //prepare trafo
-            val trafo = Trafo(point,
-                code.text.toString(),
-                name.text.toString(),
-                type.text.toString().toShortOrNull(),
-                cal,
-                field.text.toString())
-
-            dbManager.addTrafo(trafo)
-            pointGraphicsOverlay.graphics.clear()
-            popupWindow?.dismiss()
-        }
-
-        val cancelBtn = view.findViewById<Button>(R.id.cancel)
-        cancelBtn.setOnClickListener {
-            pointGraphicsOverlay.graphics.clear()
-            popupWindow?.dismiss()
-        }
+        saveTrafoPopUp.showPopUp(point)
     }
 }
---
+++ app/src/main/java/com/example/app/UI/SaveTrafoPopUp.kt
@@ -0,0 +1,70 @@
+package com.example.app.UI
+
+import android.app.Activity
+import android.content.Context
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.esri.arcgisruntime.geometry.Point
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay
+import com.example.app.Data.Trafo
+import com.example.app.Managers.DBManager
+import com.example.app.R
+import java.util.*
+
+class SaveTrafoPopUp(val context: Context,val dbManager: DBManager, val layout: ConstraintLayout, val pointGraphicsOverlay: GraphicsOverlay) {
+    var popupWindow: PopupWindow? = null
+
+    fun showPopUp(point: Point){
+        //get relevant layout
+        val inflater = context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.save_point_popup,null)
+
+        //show popupwindow TODO: Take a look at bottom sheet dialog
+        val width = LinearLayout.LayoutParams.MATCH_PARENT
+        val height = LinearLayout.LayoutParams.WRAP_CONTENT
+        popupWindow = PopupWindow(view, width, height, true)
+        popupWindow?.showAtLocation(layout, Gravity.BOTTOM,0,0)
+
+        val code = view.findViewById<EditText>(R.id.code_input)
+        val name = view.findViewById<EditText>(R.id.name_input)
+        val type = view.findViewById<EditText>(R.id.type_input)
+        val field = view.findViewById<EditText>(R.id.field_input)
+        val datePicker = view.findViewById<DatePicker>(R.id.date_input)
+
+        //set button events to save to db or close the popup
+        val saveBtn = view.findViewById<Button>(R.id.popup_close_button)
+        saveBtn.setOnClickListener {
+            //get the date
+            val month = datePicker.month
+            val day = datePicker.dayOfMonth
+            val year = datePicker.year
+
+            val cal = Calendar.getInstance()
+            cal.set(year,month,day)
+
+            //prepare trafo
+            val trafo = Trafo(point,
+                code.text.toString(),
+                name.text.toString(),
+                type.text.toString().toShortOrNull(),
+                cal,
+                field.text.toString())
+
+            dbManager.addTrafo(trafo)
+            pointGraphicsOverlay.graphics.clear()
+            popupWindow?.dismiss()
+        }
+
+        val cancelBtn = view.findViewById<Button>(R.id.cancel)
+        cancelBtn.setOnClickListener {
+            pointGraphicsOverlay.graphics.clear()
+            popupWindow?.dismiss()
+        }
+    }
+
+    fun dismiss(){
+        popupWindow?.dismiss()
+    }
+}
